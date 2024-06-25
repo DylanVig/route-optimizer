6bcc05e5a81037c6a4831a0afbbd69ccc46d91de
DylanVig <drv36@cornell.edu>
2024-06-25 14:51:29 -0400

    Added transition to intro page

--- route_optimizer_frontend/src/IntroPage/Intro.js
+++ route_optimizer_frontend/src/IntroPage/Intro.js
@@ -1,13 +1,26 @@
 import Login from '../Login/Login.js'
 import './Intro.css'
+import { useState, useEffect } from 'react'
 
 export default function Intro({ handleLogin }) {
+    
+    const [loaded, setLoaded] = useState(0); 
 
+    useEffect(() => {
+        const timer = setTimeout(() => setLoaded(1), 100);
+        return () => clearTimeout(timer);
+    }, []);
 
     return (
         <div className="intro">
-            <center>
-                <h1>Tour Guide Optimizer</h1>
+            <center style={{
+            opacity: loaded,
+            transition: "opacity 800ms ease-in",
+            filter: "drop-shadow(5px 5px 5px rgba(0, 0, 0, 0.5)",
+          }}>
+                <div className="introTitle">
+                    <h1>Tour Guide Optimizer</h1>
+                </div>
                 <Login onLogin={handleLogin} /> 
             </center>
         </div>
