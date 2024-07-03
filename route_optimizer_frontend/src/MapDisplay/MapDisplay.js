7d96fa85a9801a1f7e4ff52a9dda11688cee8602
DylanVig <drv36@cornell.edu>
2024-07-02 22:37:52 -0400

    Added Google Maps to frontend

--- route_optimizer_frontend/src/MapDisplay/MapDisplay.js
+++ route_optimizer_frontend/src/MapDisplay/MapDisplay.js
@@ -1,17 +1,52 @@
 /* global google */
 import React, { useEffect } from 'react';
 
-export default function MapDisplay() {
+export default function MapDisplay({ locations }) {
   useEffect(() => {
-    let map;
+    function initMap() {
+      const origin = { lat: 40.744450, lng: -73.993900 };
+      const destination = { lat: 40.727840, lng: -73.991539 };
 
-    async function initMap() {
-      const { Map } = await google.maps.importLibrary("maps");
+      // Ensure google.maps is available
+      if (typeof google !== 'undefined' && google.maps) {
+        const map = new google.maps.Map(document.getElementById("map"), {
+          center: origin,
+          zoom: 14,
+        });
 
-      map = new Map(document.getElementById("map"), {
-        center: { lat: -34.397, lng: 150.644 },
-        zoom: 8,
-      });
+        const markerOrigin = new google.maps.Marker({
+          map: map,
+          position: origin,
+          title: "Origin",
+        });
+
+        const markerDestination = new google.maps.Marker({
+          map: map,
+          position: destination,
+          title: "Destination",
+        });
+
+        const directionsService = new google.maps.DirectionsService();
+        const directionsRenderer = new google.maps.DirectionsRenderer();
+
+        directionsRenderer.setMap(map);
+
+        const request = {
+          origin: origin,
+          destination: destination,
+          travelMode: google.maps.TravelMode.DRIVING,
+        };
+
+        directionsService.route(request, (result, status) => {
+          if (status === google.maps.DirectionsStatus.OK) {
+            directionsRenderer.setDirections(result);
+          } else {
+            console.error(`error fetching directions ${result}`);
+          }
+        });
+      } else {
+        console.error('Google Maps library is not loaded.');
+      }
     }
 
     initMap();
